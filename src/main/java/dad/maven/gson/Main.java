3dbe88066db2c76e1e96db43852fc8f956d85a23
AohigeX <69083318+AohigeX@users.noreply.github.com>
2021-09-23 12:50:48 +0100

    Adición guardado JSON
    Se ha añadido un extra para almacenar la persona en un archivo .json en formato UTF8

--- src/main/java/dad/maven/gson/Main.java
+++ src/main/java/dad/maven/gson/Main.java
@@ -5,13 +5,18 @@ import java.util.Scanner;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.charset.Charset;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// Inicialiación
 		Scanner sc = new Scanner(System.in);
 		Persona p = new Persona();
-		
+
 		// Recogida de datos
 		System.out.print("Nombre: ");
 		p.setNombre(sc.nextLine());
@@ -26,6 +31,15 @@ public class Main {
 		Gson gson = new GsonBuilder().setPrettyPrinting().create();
 		String json = gson.toJson(p);
 		
+		// (Extra) Guardado del JSON en un archivo
+		try {
+			FileWriter fw = new FileWriter("personas.json", Charset.forName("UTF8"));
+			fw.write(json);
+			fw.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 		// Mostramos el JSON
 		System.out.println(json);
